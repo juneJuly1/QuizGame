c3fd43ac585d61b7b4ce754811eb4858ffefe412
juneJuly1 <juliobanda08@gmail.com>
2023-05-24 12:26:12 -0500

    Added counter for correct questions

--- QuizGame.py
+++ QuizGame.py
@@ -14,11 +14,14 @@ else:
 
 print('Lets play!')
 
+score = 0
+
 # Question 1
 user_answer = input('1. What does CPU stand for? ')
 q_answer = 'central processing unit'
 if user_answer.lower() in q_answer:
     print('Correct!')
+    score += 1
 else:
     print('Incorrect.')
 
@@ -27,6 +30,7 @@ user_answer = input('2. What does GPU stand for? ')
 q_answer = 'graphics processing unit'
 if user_answer.lower() in q_answer:
     print('Correct!')
+    score += 1
 else:
     print('Incorrect.')
 
@@ -35,5 +39,8 @@ user_answer = input('3. What does PSU stand for? ')
 q_answer = 'power supply unit'
 if user_answer.lower() in q_answer:
     print('Correct!')
+    score += 1
 else:
     print('Incorrect.')
+
+print('You got', score, '/3 questions correct.')
