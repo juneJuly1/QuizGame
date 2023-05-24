faf740bbe9a1b36e6cbe3e9a0f61007c9acfe083
juneJuly1 <juliobanda08@gmail.com>
2023-05-24 11:40:46 -0500

    Created Question 1

--- QuizGame.py
+++ QuizGame.py
@@ -1,7 +1,5 @@
 yes_strings = ['y', 'Y', 'yes', 'Yes']
 
-
-
 print('Welcome to the quiz!')
 
 playing = input('Do you want to play? ')
@@ -10,5 +8,13 @@ if playing not in yes_strings:
     print('No Play')
     quit()
 else:
-    print('Yes Play')
+    print('Yes Play ')
 
+print('Lets play!')
+
+user_answer = input('1. What does CPU stand for? ')
+q1_answer = ('central processing unit', 'Central Processing Unit')
+if user_answer in q1_answer:
+    print('Congrats! Question 1 response is correct!')
+else:
+    print('Sorry, Question 1 response is incorrect.')
