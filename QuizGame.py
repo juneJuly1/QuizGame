4b9c0d98778f0d52448c500b6467cc2ca3bbde0b
juneJuly1 <juliobanda08@gmail.com>
2023-05-24 11:33:10 -0500

    Welcoming user and if user wants to play

--- QuizGame.py
+++ QuizGame.py
@@ -1,1 +1,14 @@
-print('testing')
+yes_strings = ['y', 'Y', 'yes', 'Yes']
+
+
+
+print('Welcome to the quiz!')
+
+playing = input('Do you want to play? ')
+
+if playing not in yes_strings:
+    print('No Play')
+    quit()
+else:
+    print('Yes Play')
+
